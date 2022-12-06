aab755ddfa7d7f551ac02b1e5cf8b4fb23d73745
Matias Vallejos <matiasvallejosdev@outlook.com>
2022-12-06 12:26:45 -0300

    Delete default createuser

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py shell -c "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('admin@climatechallenge.com', 'admin19573')"
 python manage.py collectstatic --no-input
 python manage.py migrate
 
