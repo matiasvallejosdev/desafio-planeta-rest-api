2fd1ec993f597b8e3fc3db006f1bc93a09040e9b
Matias Vallejos <matiasvallejosdev@outlook.com>
2022-12-06 11:55:45 -0300

    Build createsuperuser

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 
-python manage.py createsuperuser
+python manage.py shell -c "from django.contrib.auth import get_user_model; get_user_model().objects.create_superuser('admin@climatechallenge.com', 'admin19573')"
